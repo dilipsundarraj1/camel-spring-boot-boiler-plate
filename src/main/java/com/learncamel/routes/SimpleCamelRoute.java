4aaaa9bb99cabcffd28f546679e638e58ced533d
Dilip.Sundarraj <Dilip.Sundarraj@target.com>
2018-02-19 21:58:32 -0600

    added the onexception

--- src/main/java/com/learncamel/routes/SimpleCamelRoute.java
+++ src/main/java/com/learncamel/routes/SimpleCamelRoute.java
@@ -1,6 +1,10 @@
 package com.learncamel.routes;
 
+import com.learncamel.alert.MailProcessor;
+import com.learncamel.exception.DataException;
+import org.apache.camel.LoggingLevel;
 import org.apache.camel.builder.RouteBuilder;
+import org.postgresql.util.PSQLException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.core.env.Environment;
@@ -21,8 +25,19 @@ public class SimpleCamelRoute extends RouteBuilder{
     @Autowired
     DataSource dataSource;
 
+    @Autowired
+    MailProcessor mailProcessor;
+
+
     @Override
     public void configure() throws Exception {
+
+        onException(PSQLException.class).log(LoggingLevel.ERROR,"PSQLException in the route ${body}")
+                .maximumRedeliveries(3).redeliveryDelay(3000).backOffMultiplier(2).retryAttemptedLogLevel(LoggingLevel.ERROR);
+
+        onException(DataException.class,RuntimeException.class).log(LoggingLevel.ERROR, "DataException in the route ${body}")
+                .process(mailProcessor);
+
 
         from("{{fromRoute}}")
                     .log("Current Environment is "+ environment.getProperty("message"))
